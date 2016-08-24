a6554b4fe1dc9954e0f2480598505e5398a14e11
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-24 14:12:34 +0900

    Create Validate.ts

---
+++ Validate.ts
@@ -0,0 +1,5 @@
+export default class Validate {
+  static isEmail(string: string) :boolean {
+      return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(string);
+  }
+}
