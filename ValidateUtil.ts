b6e1758493f891a99e7dee53c20a2888bc592a6c
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-25 08:03:02 +0900

    Update and rename Validate.ts to ValidateUtil.ts

--- ValidateUtil.ts
+++ ValidateUtil.ts
@@ -1,4 +1,4 @@
-export default class Validate {
+export default class ValidateUtil {
   static isEmail(string: string) :boolean {
       return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(string);
   }
